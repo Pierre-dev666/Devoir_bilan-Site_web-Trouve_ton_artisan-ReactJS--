0f71eb12fa75ffb5733716a6764413f36ec82e9a
pierre <pierre.couderc06@gmail.com>
2025-07-02 06:04:03 +0200

    modifs Card + CraftsmanSheet

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from "react"
+import { Link } from 'react-router-dom'
 
 function Card() {
-    const [data, setData] = useState([])
+    const [user, setUsers] = useState([]);
+    const getUserInformation = async () => {
+        const res = await fetch("http://localhost:3000/users/test@test.fr");
+        const json = await res.json();
+        setUsers(json);
+    }
     useEffect(() => {
-        fetch('http://localhost/phpmyadmin/index.php?route=/database/structure&db=trouve_ton_artisan')
-            .then(res => res.json())
-            .then(data => res.setData(data))
-            .then(err => console.log(err));
-    }, [])
+        getUserInformation();
+
+    }, []);
 
     return (
         <div id='card'>
-            <table>
-                
-                {data.map((d, i) => (
-                    <div key={i}>
-                        <p>{d.id_artisan}</p>
-                    </div>,
-                    <div>
-                    <p>test</p>
+     
+                <div id="card_first-part">
+                    <Link to="/CraftsmanSheetPage" ><button>Bâtiment</button></Link>
                 </div>
-                ))}
-
-            </table>
+                <div id="card_second-part">
+                    <p>Note :{user.name}</p>
+                </div>
+                <div id="card_third-part">
+                    <p>{user.name}</p>
+                </div>
+                <div id="card_fourth-part">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11441.390831487402!2d0.6291456000000001!3d44.1999046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12abb31daed28661%3A0x2ecfc644f277ef52!2sCIN%C3%89MA%20CGR%20Agen!5e0!3m2!1sfr!2sfr!4v1751423113600!5m2!1sfr!2sfr"></iframe>
+                </div>
+           
         </div>
     )
 }
--- src/components/CraftsmanSheet.jsx
+++ src/components/CraftsmanSheet.jsx
@@ -1,27 +1,54 @@
+import React, { useEffect, useState } from "react"
+
 function CraftsmanSheet() {
 
+    const [user, setUsers] = useState([]);
+    const getUserInformation = async () => {
+        const res = await fetch("http://localhost:3000/users/test@test.fr");
+        const json = await res.json();
+        setUsers(json);
+    }
+    useEffect(() => {
+        getUserInformation();
+
+    }, []);
     return (
         <div id='craftsmansheet'>
-            <div id='craftsmansheet_first-part'>
-                <p>blabla</p>
+            <div id="craftsmansheet_first-bloc">
+                <div id='craftsmansheet_first-part'>
+                    <p>blabla</p>
+                </div>
+                <div id='craftsmansheet_second-part'>
+                    <p>blabla</p>
+                </div>
             </div>
-            <div id='craftsmansheet_second-part'>
-                <p>blabla</p>
-            </div>
-            <div id='craftsmansheet_third-part'>
-                <p>blabla</p>
-            </div>
-            <div id='craftsmansheet_fourth-part'>
-                <p>blabla</p>
-            </div>
-            <div id='craftsmansheet_fifth-part'>
-                <p>blabla</p>
-            </div>
-            <div id='craftsmansheet_sixth-part'>
-                <p>blabla</p>
-            </div>
-            <div id='craftsmansheet_seventh-part'>
-                <p>blabla</p>
+            <div id="craftsmansheet_second-bloc">
+                <div id='craftsmansheet_third-part'>
+                    <p>blabla</p>
+                </div>
+                <div id='craftsmansheet_fourth-part'>
+                    <p>blabla</p>
+                </div>
+                <div id='craftsmansheet_fifth-part'>
+                    <p>blabla</p>
+                </div>
+                <div id='craftsmansheet_sixth-part'>
+                    <p>blabla</p>
+                </div>
+                <form id='craftsmansheet_seventh-part'>
+                    <fieldset id="form">
+                        <input type="text" placeholder="Nom..." id="name" required />
+                        <label htmlFor="name"></label>
+                        <input type="text" placeholder="Adresse mail..." id="mail" required />
+                        <label htmlFor="mail"></label>
+                        <input type="text" placeholder="Objet du message..." id="object" required />
+                        <label htmlFor="object"></label>
+                        <textarea name="demand" id="demand" cols="60" rows="10"
+                            placeholder="Saisissez votre message ici" required></textarea>
+                    </fieldset>
+                    <input type="submit" value="Envoyer" id="validate"/>
+                        <label htmlFor="validate"></label>
+                </form>
             </div>
         </div>
 
